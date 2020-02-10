8c663c17ba207266ab46174f51499a24ec4e288c
Yip Han Edgar Yong <s10179383@connect.np.edu.sg>
2020-02-10 17:54:36 +0800

    nothing done again

---
+++ SE_Assignment/SE_Assignment/ManagerFunctions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Assignment
+{
+    class ManagerFunctions
+    {
+        //Test whether this shit work
+    }
+}
