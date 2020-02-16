78146042214baa8ed418607101165b8234c0f03e
Yongyiphan <edgar.yong.yip.han@gmail.com>
2020-02-16 20:57:40 +0800

    Checkout to YH

---
+++ SE_Assignment/SE_Assignment/Branch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SE_Assignment
+{
+    class Branch
+    {
+        public string name { get; set; }
+
+        public Branch(string name)
+        {
+            this.name = name;
+        }
+    }
+}
